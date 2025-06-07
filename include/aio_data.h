5389460c298361949189f84824feb837624817ea
alic3 <alic3dev@gmail.com>
2025-06-07 15:06:03 -0700

    use->{`cer0_oscillator`}|implement->{`aio_data`}

---
+++ include/aio_data.h
@@ -0,0 +1,17 @@
+#ifndef __aio_data_h
+#define __aio_data_h
+
+#include <cer0.h>
+
+struct aio_data {
+  unsigned char initialized;
+
+  FILE* file_input;
+
+  float* note_table;
+  unsigned int length_note_table;
+
+  struct cer0_oscillator oscillator;
+};
+
+#endif
