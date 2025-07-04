001c00d8f3e418cff7c39fe86bcdf210ce644261
alic3 <alic3dev@gmail.com>
2025-07-03 19:10:11 -0700

    `ao_print_usage.[h|c]`:implement

---
+++ sources/ao_print_usage.c
@@ -0,0 +1,22 @@
+#include <ao_print_usage.h>
+
+#include <stdio.h>
+
+void ao_print_usage(
+  unsigned char to_error_stream
+) {
+  FILE* stream_output = (
+    to_error_stream == 0
+    ? stdout
+    : stderr
+  );
+
+  fprintf(
+    stream_output,
+    "usage: ao [?parameters] [...paths_to_file_inputs]\n"
+    "parameters:\n"
+    "  -o, -e, --export [output_path]\n"
+    "    --play : plays audio while exporting (default is no audio output when exporting)\n"
+    "  --help : displays usage information\n"
+  );
+}
