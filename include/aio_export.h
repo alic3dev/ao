64a0a1cb6f9422e65ef07f37344e6ecbe6b4cf67
alic3 <alic3dev@gmail.com>
2025-07-09 23:56:48 -0700

    `aio_export`:implement

---
+++ include/aio_export.h
@@ -0,0 +1,21 @@
+#ifndef __aio_export_h
+#define __aio_export_h
+
+#include <aio_data.h>
+
+#include <pthread.h>
+#include <stdio.h>
+
+pthread_mutex_t mutex_exporting;
+pthread_mutex_t mutex_export_write;
+
+int aio_export_write(
+  FILE*,
+  float
+);
+
+void aio_export_data(
+  struct aio_data*
+);
+
+#endif
